69cf9b3fd7944c8110a585b5fddbe3eae3f296fd
Marek Marecki <marekjm@ozro.pw>
2018-11-25 13:58:38 +0100

    Changed defaults for VIUAC_LIBRARY_PATH

--- src/env.py
+++ src/env.py
@@ -1,4 +1,10 @@
 import os
 
 DEFAULT_OUTPUT_DIRECTORY = os.environ.get('VIUAC_OUTPUT_DIRECTORY', './build/_default')
-VIUAC_LIBRARY_PATH = os.environ.get('VIUAC_LIBRARY_PATH', DEFAULT_OUTPUT_DIRECTORY).split(':')
+
+VIUAC_LIBRARY_PATH = (
+    tuple(filter(lambda each: each.strip(),
+        os.environ.get('VIUAC_LIBRARY_PATH', '').split(':')
+    ))
+    + (DEFAULT_OUTPUT_DIRECTORY,)
+)
