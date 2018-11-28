b732baa707b7a7db4d2e2784dc4a3c32bfc85916
Marek Marecki <marekjm@ozro.pw>
2018-11-28 07:27:30 +0100

    Fix abs example

--- sample/abs.lisp
+++ sample/abs.lisp
@@ -1,12 +1,14 @@
 (let abs (x) (
     (let negative (< (x 0)))
+    (print (negative))
     (if negative
         (* (x -1))
-        (x))
+        x)
 ))
 
 (let main () (
     (let i -3)
-    (print ((abs (b))))
+    (print (i))
+    (print ((abs (i))))
     0
 ))
