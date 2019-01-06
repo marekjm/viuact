bea59a7b78a7595e39da1f62c298d9463d5989f6
Marek Marecki <marekjm@ozro.pw>
2019-01-06 13:30:09 +0100

    Fix: compile the dependency first
    Compiling dependencies inside the same script will prevent me from
    forgetting about compiling them manually.

--- ex/separate_module.sh
+++ ex/separate_module.sh
@@ -12,6 +12,9 @@ fi
 FILE_NAME=separate_module
 
 if [[ $MODE == 'make' ]]; then
+    echo "compiling dependencies of $FILE_NAME..."
+    ./src/viuact-cc.py --mode module ex/External.lisp
+
     echo "compiling $FILE_NAME..."
     ./src/viuact-cc.py --mode exec ex/$FILE_NAME.lisp
     ./src/viuact-opt.py build/_default/$FILE_NAME.asm
