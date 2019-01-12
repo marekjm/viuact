d6cf5f705caafafeaf2a41adcd8334e80ddde639
Marek Marecki <marekjm@ozro.pw>
2019-01-12 12:08:28 +0100

    Fix simple message exchange test

--- ex/simple_message_exchange.lisp
+++ ex/simple_message_exchange.lisp
@@ -1,7 +1,7 @@
 (let sender (receivers_pid message) {
     (print "sender: sends a message...")
     (Std.Actor.send receivers_pid message)
-    (print "sender: sent a message...")
+    (print "sender: sent a message!")
     0
 })
 
@@ -10,13 +10,11 @@
     (let x (Std.Actor.receive))
     (print "receiver: got a message!")
     (print x)
-    (print "receiver: done")
     0
 })
 
 (let main () {
     (let r (actor receiver))
     (sender r "Hello World!")
-    (print "Hello XXX")
     0
 })
