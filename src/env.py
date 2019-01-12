8b97e771968b622c66b442bab2f589d5ba66d694
Marek Marecki <marekjm@ozro.pw>
2019-01-12 15:27:53 +0100

    Provide machinery that makes it possible to import FFI modules

--- src/env.py
+++ src/env.py
@@ -1,12 +1,14 @@
 import os
 
 DEFAULT_OUTPUT_DIRECTORY = os.environ.get('VIUAC_OUTPUT_DIRECTORY', './build/_default')
+STDLIB_HEADERS_DIRECTORY = os.environ.get('VIUAC_STDLIB_HEADERS', './stdlib')
 
 VIUAC_LIBRARY_PATH = (
     tuple(filter(lambda each: each.strip(),
         os.environ.get('VIUAC_LIBRARY_PATH', '').split(':')
     ))
     + (DEFAULT_OUTPUT_DIRECTORY,)
+    + (STDLIB_HEADERS_DIRECTORY,)
 )
 
 VIUA_ASM_PATH = os.environ.get('VIUA_ASM_PATH', '../core/viuavm/build/bin/vm/asm')
