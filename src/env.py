09c8bde954976248afe6d87fa6c6858d62386282
Marek Marecki <marekjm@ozro.pw>
2019-01-12 16:49:15 +0100

    Attempt to check validity of the environment variables
    Check if the files and directories they point to exist.

--- src/env.py
+++ src/env.py
@@ -3,15 +3,35 @@ import os
 DEFAULT_OUTPUT_DIRECTORY = os.environ.get('VIUAC_OUTPUT_DIRECTORY', './build/_default')
 STDLIB_HEADERS_DIRECTORY = os.environ.get('VIUAC_STDLIB_HEADERS', './stdlib')
 
-VIUAC_LIBRARY_PATH = (
+
+class Required_environment_variable_not_set(Exception):
+    pass
+class Misconfigured_environment_variable(Exception):
+    pass
+
+
+if os.environ.get('VIUA_LIBRARY_PATH') is None:
+    raise Required_environment_variable_not_set('VIUA_LIBRARY_PATH')
+
+
+VIUAC_LIBRARY_PATH = tuple(filter(os.path.isdir, (
     tuple(filter(lambda each: each.strip(),
         os.environ.get('VIUAC_LIBRARY_PATH', '').split(':')
     ))
     + (DEFAULT_OUTPUT_DIRECTORY,)
     + (STDLIB_HEADERS_DIRECTORY,)
-)
+)))
+if (not VIUAC_LIBRARY_PATH) and os.environ.get('VIUAC_LIBRARY_PATH') is None:
+    raise Required_environment_variable_not_set('VIUAC_LIBRARY_PATH')
+if (not VIUAC_LIBRARY_PATH) and os.environ.get('VIUAC_LIBRARY_PATH') is not None:
+    raise Misconfigured_environment_variable('VIUAC_LIBRARY_PATH')
+
 
 VIUA_ASM_PATH = os.environ.get('VIUA_ASM_PATH', '../core/viuavm/build/bin/vm/asm')
+if (not os.path.isfile(VIUA_ASM_PATH)) and os.environ.get('VIUA_ASM_PATH') is None:
+    raise Required_environment_variable_not_set('VIUA_ASM_PATH')
+if (not os.path.isfile(VIUA_ASM_PATH)) and os.environ.get('VIUA_ASM_PATH') is not None:
+    raise Misconfigured_environment_variable('VIUA_ASM_PATH')
 
 
 def bool_of_string(s):
