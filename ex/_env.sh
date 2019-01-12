e4ce3e575331e2d0f8d9a444d65ac1e636d5495c
Marek Marecki <marekjm@ozro.pw>
2019-01-12 15:30:55 +0100

    Add environment setup script

---
+++ ex/_env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This file contains the exports of environment variables that are neccessary
+# for correct operation of the Viuact compiler.
+
+export VIUA_LIBRARY_PATH=../core/viuavm/build/stdlib
+export VIUAC_LIBRARY_PATH=../core/viuavm/build/stdlib
