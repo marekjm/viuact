5af4f0e32ff30a3a39d7dbf29fb65c41a2d6d6ef
Marek Marecki <marekjm@ozro.pw>
2019-01-19 22:21:32 +0100

    Use kernel paths from environment

--- ex/chat_server.sh
+++ ex/chat_server.sh
@@ -18,5 +18,5 @@ if [[ $MODE == 'make' ]]; then
 fi
 if [[ $MODE == 'run' ]]; then
     echo "running $FILE_NAME..."
-    gdb --args ../core/viuavm/build/bin/vm/kernel build/_default/$FILE_NAME.bc ${@:2}
+    $VIUA_VM_KERNEL_PATH build/_default/$FILE_NAME.bc ${@:2}
 fi
