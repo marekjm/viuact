35c60d9195370a8cd3ca8d6faee3e9a94f797740
Marek Marecki <marekjm@ozro.pw>
2019-03-06 23:39:12 +0100

    Add message router for basic multi-user chat

--- ex/webchat.lisp
+++ ex/webchat.lisp
@@ -13,23 +13,59 @@
     })))
 })
 
-(let user_loop (client) {
+(let user_loop (client router) {
     (let message (receive_message client))
-    (Websocket.write client (Std.JSON.to_json message))
-    (tailcall user_loop client)
+
+    (let received (struct))
+    (:= received.event 2)
+    (:= received.value message)
+    (Std.Actor.send router received)
+
+    (tailcall user_loop client router)
     0
 })
 
-(let user_establish (client) {
+(let user_establish (client router) {
     (Websocket.handshake client)
-    (tailcall user_loop client)
+
+    (let register (struct))
+    (:= register.event 1)
+    (:= register.value (Std.Actor.self))
+    (Std.Actor.send router register)
+
+    (tailcall user_loop client router)
     0
 })
 
-(let accept_loop (sock) {
+(let accept_loop (sock router) {
     (let client (Std.Posix.Network.accept sock))
-    (actor user_establish client)
-    (tailcall accept_loop sock)
+    (actor user_establish client router)
+    (tailcall accept_loop sock router)
+    0
+})
+
+(let send_to_all_clients (clients message) {
+    (print clients)
+    (print message)
+    0
+})
+
+(let message_router_impl (clients) {
+    (let msg (Std.Actor.receive))
+    (if (= msg.event 1) {
+        (Std.Vector.push clients (^ msg.value))
+        (print clients)
+        (tailcall message_router_impl clients)
+    } (if (= msg.event 2) {
+        (send_to_all_clients clients (^ msg.value))
+        (tailcall message_router_impl clients)
+    } (tailcall message_router_impl clients)))
+})
+
+(let message_router (back) {
+    (let clients (vector))
+    (Std.Actor.send back (Std.Actor.self))
+    (tailcall message_router_impl clients)
     0
 })
 
@@ -38,7 +74,10 @@
     (Std.Posix.Network.bind sock "127.0.0.1" 9090)
     (Std.Posix.Network.listen sock 16)
 
-    (accept_loop sock)
+    (actor message_router (Std.Actor.self))
+    (let router (Std.Actor.receive))
+
+    (accept_loop sock router)
 
     0
 })
