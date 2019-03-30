bde67258d27c95ce2227f3efb3cb7e5e5a114582
Marek Marecki <marekjm@ozro.pw>
2019-03-30 10:46:14 +0100

    Remove integer example is just remove integer example
    Filter will get its own example.

--- ex/remove_integer.sh
+++ ex/remove_integer.sh
@@ -14,7 +14,7 @@ FILE_NAME=remove_integer
 if [[ $MODE == 'make' ]]; then
     echo "compiling $FILE_NAME..."
     ./cc.py --mode exec ex/$FILE_NAME.lisp
-    ./opt.py build/_default/$FILE_NAME.asm
+    VIUAC_ASM_FLAGS='-Wunused-value' ./opt.py build/_default/$FILE_NAME.asm
 fi
 if [[ $MODE == 'run' ]]; then
     echo "running $FILE_NAME..."
